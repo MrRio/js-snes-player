9ce6c9454984dd2005df3ee8f12309e450fd890c
James Hall <james@snapshotmedia.co.uk>
2012-08-28 12:27:13 +0100

    Tidy, add console output

--- js/js-snes-player.js
+++ js/js-snes-player.js
@@ -10,15 +10,13 @@ var jsSNESPlayer = function() {
 
 	return {
 		init: function() {
-			
-
 			//run();
-			
+			//console.log(window);
 
-			console.log(window);
-
+			console.log('Make new SPC instance...');
 			var snes_spc = _spc_new();
 
+			console.log('Load filter...');
 			var filter = _spc_filter_new();
 			if (!snes_spc || !filter) {
 				alert('Failed to load');
@@ -29,22 +27,30 @@ var jsSNESPlayer = function() {
 
 				var spc = allocate(SongData, 'i8', ALLOC_STACK);
 
+				console.log('Load SPC data into pointer...');
+
+
 				var spc_size = SongData.length;
-				console.log(spc_size);
+
+				console.log('SPC size is ' + spc_size);
+
+				console.log('Loading SPC');
 				var load = _spc_load_spc(snes_spc, spc, spc_size);
 				error(Pointer_stringify(load));
+				console.log('SPC file loaded');
 
 				//_spc_set_tempo(snes_spc, 180);
 				_spc_set_output();
 				_spc_clear_echo(snes_spc);
 				_spc_filter_clear(filter);
 
+				console.log('Creating audio sink...');
+				var buf = allocate('', 'i8', ALLOC_STACK);
 
 				var bufferSize = 240000, sink = Sink();
 				var proxy = sink.createProxy(bufferSize);
 				proxy.on('audioprocess', function(buffer, channelCount){
 
-					var buf = allocate('', 'i8', ALLOC_STACK);
 					var retval = _spc_play(snes_spc, bufferSize, buf);
 					_spc_filter_run(filter, buf, bufferSize);
 
@@ -59,7 +65,7 @@ var jsSNESPlayer = function() {
 
 						}
 					}
-					console.log('Buffer');
+					console.log('Send audio to buffer... Memory points from HEAP ' + i + ' to ' + (i + buf));
 				}, 2, null, 24000);
 
 			}
